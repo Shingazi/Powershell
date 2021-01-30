8177ab88d9390247a2900e19167d8704b1a99509
Shingazi <33381081+Shingazi@users.noreply.github.com>
2021-01-30 18:29:41 +0100

    Add files via upload

---
+++ Ballon_Notification_Pokemon_Buddy_Timer.ps1
@@ -0,0 +1,21 @@
+﻿### Powershell Ballon Notification ###
+Add-Type -AssemblyName System.Windows.Forms
+$global:balmsg = New-Object System.Windows.Forms.NotifyIcon
+$path = (Get-Process -id $pid).Path
+$balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+$balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
+$balmsg.BalloonTipTitle = ‘❤Pokémon❤'
+$balmsg.BalloonTipText = "!Pokémon Buddy Aktion vornehmen!"
+$balmsg.Visible = $true
+Start-Sleep -Seconds 1800
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 1800
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 1800
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 1800
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 1800
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 1800
+$balmsg.ShowBalloonTip(2000)
---
+++ Ballon_Notification_Pokemon_Spin_Stop_Timer.ps1
@@ -0,0 +1,33 @@
+﻿### Powershell Ballon Notification ###
+Add-Type -AssemblyName System.Windows.Forms
+$global:balmsg = New-Object System.Windows.Forms.NotifyIcon
+$path = (Get-Process -id $pid).Path
+$balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+$balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
+$balmsg.BalloonTipTitle = ‘❤Pokémon❤'
+$balmsg.BalloonTipText = "!Pokémon Stop drehen!"
+$balmsg.Visible = $true
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
+Start-Sleep -Seconds 300
+$balmsg.ShowBalloonTip(2000)
