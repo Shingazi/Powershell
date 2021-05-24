fc53e21adb8a6d529b5c73a9382ace4209c1235e
Vertrauensstellung <33381081+Vertrauensstellung@users.noreply.github.com>
2021-05-24 18:14:28 +0200

    Create Pull_Latest_Pogo_Version_APK_MIRROR.ps1

---
+++ Pull_Latest_Pogo_Version_APK_MIRROR.ps1
@@ -0,0 +1,3 @@
+﻿$content = Invoke-WebRequest -Uri https://www.apkmirror.com/apk/niantic-inc/pokemon-go/
+$content.Links | Where-Object {$_.href -like "/apk/niantic-inc/pokemon-go/pokemon-go*"} | Select-Object href -ExpandProperty href -First 1
+$content.Links | Where-Object {$_.href -like "/apk/niantic-inc/pokemon-go/pokemon-go*"} | Select-Object innerHTML -ExpandProperty innerHTML -First 1
