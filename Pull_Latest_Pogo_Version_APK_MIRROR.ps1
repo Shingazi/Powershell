178b18151b7ee80fdf02557c3b13c326481815da
Vertrauensstellung <33381081+Vertrauensstellung@users.noreply.github.com>
2021-05-24 18:45:37 +0200

    Update Pull_Latest_Pogo_Version_APK_MIRROR.ps1

--- Pull_Latest_Pogo_Version_APK_MIRROR.ps1
+++ Pull_Latest_Pogo_Version_APK_MIRROR.ps1
@@ -1,5 +1,11 @@
 ﻿$content = Invoke-WebRequest -Uri https://www.apkmirror.com/apk/niantic-inc/pokemon-go/
 ### Get Url ###
-$content.Links | Where-Object {$_.href -like "/apk/niantic-inc/pokemon-go/pokemon-go*"} | Select-Object href -ExpandProperty href -First 1
+$apkurl = $content.Links | Where-Object {$_.href -like "/apk/niantic-inc/pokemon-go/pokemon-go*"} | Select-Object href -ExpandProperty href -First 1
 ### Get Version Name ###
-$content.Links | Where-Object {$_.href -like "/apk/niantic-inc/pokemon-go/pokemon-go*"} | Select-Object innerHTML -ExpandProperty innerHTML -First 1
+$version = $content.Links | Where-Object {$_.href -like "/apk/niantic-inc/pokemon-go/pokemon-go*"} | Select-Object innerHTML -ExpandProperty innerHTML -First 1
+### Extract latest 7 characters ###
+$versionnumber = $version.Substring($version.Length -7, 7)
+### arm-v7a ###
+start https://www.apkmirror.com$url/pokemon-go-$versionnumber-2-android-apk-download/download/?forcebaseapk
+### arm64-v8a ###
+start https://www.apkmirror.com$url/pokemon-go-$versionnumber-3-android-apk-download/download/?forcebaseapk
